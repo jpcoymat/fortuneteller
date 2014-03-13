b1dc78c8c08d90b9a487d86871d6af05ba7a9da0
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-03-13 20:28:59 +0000

    changes

---
+++ app/controllers/product_location_assignments_controller.rb
@@ -0,0 +1,53 @@
+class ProductLocationAssignmentsController < ApplicationController
+
+  def index
+    @product_location_assignments = User.find(session[:user_id]).product_location_assignments
+  end
+
+  def show
+    @product_location_assignment = ProductLocationAssignment.find(params[:id]
+  end
+
+  def edit
+    @organization = User.find(session[:user_id]).organization
+    @product_location_assignment = ProductLocationAssignment.find(params[:id])
+    @location = @organization.locations
+    @products = @organization.products
+  end
+
+  def update
+    @product_location_assignment = ProductLocationAssignment.find(params[:id])
+    if @product_location_assignment.update_attributes(params[:product_location_assignment])
+      redirect_to product_location_assignment_path(@product_location_assignment)
+    else
+      @organization = User.find(session[:user_id]).organization
+      @product_location_assignment = ProductLocationAssignment.find(params[:id])
+      @location = @organization.locations
+      @products = @organization.products
+      render action: "edit"
+    end
+  end
+
+  def new
+    @product_location_assignment = ProductLocationAssignment.new
+    @organization = User.find(session[:user_id]).organization
+    @product_location_assignment = ProductLocationAssignment.find(params[:id])
+    @location = @organization.locations
+    @products = @organization.products
+  end
+
+  def create
+    @product_location_assignment = ProductLocationAssignment.new(params[:product_location_assignment])
+    if @product_location_assignment.save
+      redirect_to product_location_assignment_path(@product_location_assignment)
+    else
+      @organization = User.find(session[:user_id]).organization
+      @product_location_assignment = ProductLocationAssignment.find(params[:id])
+      @location = @organization.locations
+      @products = @organization.products
+      render action: "new"
+    end
+  end
+
+end
+
