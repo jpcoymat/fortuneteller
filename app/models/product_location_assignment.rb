555387b8aa35e2a9a7350e70b199932326a44d1b
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-03-18 17:52:55 +0000

    changes

--- app/models/product_location_assignment.rb
+++ app/models/product_location_assignment.rb
@@ -11,12 +11,10 @@ class ProductLocationAssignment
   validates :product_id, uniqueness: {scope: :location_id}
 
   def create_inventory_position
-    @inventory_position = InventoryPosition.new
-    @inventory_position.product = self.product
-    @inventory_position.location = self.location
-    @inventory_position.on_hand_quantity, @inventory_position.forecasted_quantity, @inventory_position.on_order_quantity, @inventory_position.in_transit_quantity, @inventory_position.allocated_quantity = 0,0,0,0,0
-    @inventory_position.save
-    @inventory_position.create_projections
+    @inventory_position = InventoryPosition.new(product: self.product, location: self.location, on_hand_quantity: 0)
+    if @inventory_position.save
+      @inventory_position.create_projections
+    end
   end
 
 end
