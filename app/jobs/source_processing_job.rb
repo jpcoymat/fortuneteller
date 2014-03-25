be7835c67f34959b1a722a8e6885cc5678eb6747
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-03-25 23:36:06 +0000

    changes

--- app/jobs/source_processing_job.rb
+++ app/jobs/source_processing_job.rb
@@ -5,10 +5,13 @@ class SourceProcessingJob
   attr_accessor :inventory_position
 
   def self.perform(movement_source_id)
+    write_to_log("Here we go ...")
     @movement_source = MovementSource.find(movement_source_id)
     unless @movement_source.nil?
+      write_to_log("Found Movement Source")
       case @movement_source.class
         when ShipLine
+          write_to_log("I am a ship line")
           process_ship_line(@movement_source)
         when OrderLine
           process_order_line(@movement_source)
@@ -19,13 +22,16 @@ class SourceProcessingJob
   end
 
   def self.process_ship_line(ship_line)
+    write_to_log("process ship line starting ..")
     origin_inventory_position = origin_position(ship_line)
     unless origin_inventory_position.nil?
+      write_to_log("Found origin position: Product: " + origin_inventory_position.product.name + " - Location: " + origin_inventory_position.location.name) 
       origin_inventory_position.on_hand_quantity -= ship_line.quantity
       origin_inventory_position.reset_projections
     end
     destination_inventory_position = destination_position(ship_line) 
     unless destination_inventory_position.nil?
+     write_to_log("Found destination position: Product: " + origin_inventory_position.product.name + " - Location: " + origin_inventory_position.location.name)
      po_line = ship_line.parent_movement_source
      unless po_line.nil?
        position = destination_inventory_position.inventory_projections.where(projected_for: po_line.eta).first
@@ -33,9 +39,9 @@ class SourceProcessingJob
          position.on_order_quantity -= ship_line.quantity
        end
      end
-     position = destination_inventory_position.inventory_projections.where(projected_for: ship_line.eta).first
-     unless position.nil?
-       position.in_transit_quantity += ship_line.quantity
+     projection = destination_inventory_position.inventory_projections.where(projected_for: ship_line.eta).first
+     unless projection.nil?
+       projection.in_transit_quantity += ship_line.quantity
      end 
      destination_inventory_position.save
      destination_inventory_position.reset_projections
@@ -56,5 +62,8 @@ class SourceProcessingJob
     inventory_position = InventoryPosition.where(location_id: movement_source.destination_location_id, product_id: movement_source.product_id).first
   end
 
+  def self.write_to_log(log_message)
+    File.open("new_movement_sources_log.log",'a') {|f| f.write(log_message)} 
+  end
 
 end
