9fd673e498c46d62f0945507e8630b35e59e6362
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-03-31 20:59:35 +0000

    changes

--- app/controllers/order_lines_controller.rb
+++ app/controllers/order_lines_controller.rb
@@ -3,25 +3,28 @@ class OrderLinesController < ApplicationController
   before_action :set_order_line, only: [:show,:edit,:update,:destroy]
 
   def index
+    @user = User.find(session[:user_id])
+    @order_lines = OrderLine.where(organization_id: @user.organization_id).all
   end
 
   def show
   end
 
   def edit
+    @user = User.find(session[:user_id])
+    @products = @user.organization.products
+    @locations = @user.organization.locations
   end
 
   def create
     @order_line = OrderLine.new(params[:order_line])
     if @order_line.save
-      if @order_line.requires_allocation?
-        @inventory_position = InventoryPosition.where(location_id: @order_line.origin_location_id, product_id: @order_line.product_id).first
-        @inventory_position.allocate(@order_line)
-      end
-      if @order_line.requires_on_order?
-        @inventory_position = InventoryPosition.where(location_id: @order_line.destination_location_id, product_id: @order_line.product_id).first
-        @inventory_position.check_on_order(@order_line)
-      end
+      Resque.enqueue(SourceProcessingJob, @order_line.id.to_s)
+      flash[:notice] = "Order Line has been created succesfully and queued for processing."
+      redirect_to order_lines_path
+    else
+      flash[:notice] = "Error creating Order Line"
+      render action: "new"
     end
   end
 
@@ -30,6 +33,13 @@ class OrderLinesController < ApplicationController
 
   def destroy
   end
+
+  def new
+    @user = User.find(session[:user_id])
+    @products = @user.organization.products
+    @locations = @user.organization.locations
+  end
+
 
   private
     
