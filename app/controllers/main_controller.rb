9d7f565606ce8859ca7b33b25994cfe9ef5a5305
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-05-09 20:28:17 +0000

    map stuff

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,11 @@ class MainController < ApplicationController
 
   def index
     @user = User.find(session[:user_id])
+    @locations = @user.organization.locations
+    @map_data = []
+    @locations.each do |location|
+       @map_data << [location.city, location.aggregate_quantity("on_hand_quantity")]
+    end
   end
 
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -15,11 +15,32 @@ class Location
 
   validates :name, :code, :city, :country, presence: true
   validates_uniqueness_of :code, scope: :organization_id
+  has_many :inventory_positions
+  has_many :product_location_assignments
 
   after_initialize :activate
 
   def activate
     self.is_active = true
   end
+
+  def aggregate_quantity(inventory_bucket)
+    total_quantity = 0
+    self.inventory_positions.each do |position|
+      if inventory_bucket = "on_hand_quantity"  
+        total_quantity += position.on_hand_quantity 
+      else
+        total_quantity += position.projection_aggregate(inventory_bucket)
+      end 
+    end
+    total_quantity	
+  end
+  
+  def carrying_capacity
+    @carrying_capacity = 0
+    self.product_location_assignments {|pla| @carrying_capacity += pla.maximum_quantity}
+    @carrying_capacity
+  end
+
 
 end
