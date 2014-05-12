2ed0df3574ae61e121e87557265a07c7e7d69777
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-05-12 19:47:42 +0000

    graph changes

--- app/controllers/inventory_positions_controller.rb
+++ app/controllers/inventory_positions_controller.rb
@@ -10,30 +10,37 @@ class InventoryPositionsController < ApplicationController
     if request.post?
       @inventory_position = InventoryPosition.where(search_params).first
       if @inventory_position
+        @product_location_assignment = ProductLocationAssignment.where(product: @inventory_position.product, location: @inventory_position.location).first
         @data = []
+        min_quantity = []
         on_hand_data = []
 	on_order_data =[]
 	in_transit_data = []
 	allocated_data = []
 	forecasted_data = []
 	available_data = []
+        max_quantity = []
         search_range_start = begin_date 
         search_range_end = end_date || search_range_start + @user.organization.days_to_project.days
         @projections = @inventory_position.inventory_projections.where(:projected_for.gte => search_range_start, :projected_for.lte => search_range_end).all
         @projections.each do |ip| 
+          min_quantity << [ip.projected_for.to_formatted_s(:short),  @product_location_assignment.minimum_quantity]
 	  on_hand_data << [ip.projected_for.to_formatted_s(:short), ip.on_hand_quantity]
           available_data << [ip.projected_for.to_formatted_s(:short), ip.available_quantity]
 	  on_order_data << [ip.projected_for.to_formatted_s(:short), ip.on_order_quantity]
  	  in_transit_data << [ip.projected_for.to_formatted_s(:short), ip.in_transit_quantity]
 	  allocated_data << [ip.projected_for.to_formatted_s(:short), ip.allocated_quantity]
 	  forecasted_data << [ip.projected_for.to_formatted_s(:short), ip.forecasted_quantity]
+          max_quantity << [ip.projected_for.to_formatted_s(:short), @product_location_assignment.maximum_quantity]
         end
-        @data << {name: "On Hand Quantity", data: on_hand_data}
-	@data << {name: "On Order Quantity", data: on_order_data}
-	@data << {name: "In Transit Quantity", data: in_transit_data}
-	@data << {name: "Allocated Quantity", data: allocated_data}
-	@data << {name: "Available Quantity", data: available_data}
-	@data << {name: "Forecasted Quantity", data: forecasted_data}
+        @data << {name: "Min", data: min_quantity}
+        @data << {name: "On Hand", data: on_hand_data}
+	@data << {name: "In Transit", data: in_transit_data}
+	@data << {name: "On Order", data: on_order_data}
+	@data << {name: "Allocated", data: allocated_data}
+	@data << {name: "Planned Consumption", data: forecasted_data}
+	@data << {name: "Available", data: available_data}
+        @data << {name: "Max", data: max_quantity}
       end
     end
   end
