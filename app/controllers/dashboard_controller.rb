3ccf8d4fb4fbcac7a6c191f1edaa31ded35c0c34
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-05-15 20:32:04 +0000

    changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,7 +7,7 @@ class DashboardController < ApplicationController
     @locations = @user.organization.locations
     @data_array_string = ""
     @locations.each do |location|
-        @data_array_string += "[" + location.latitude.to_s + ", " + location.longitude.to_s +  ", " + location.inventory_exceptions.count.to_s + ", " + location.name + "]"
+        @data_array_string += "[" + location.latitude.to_s + ", " + location.longitude.to_s +  ", " + location.inventory_exceptions.count.to_s + ", '" + location.name + "']"
         @data_array_string += "," unless @locations.last == location			
     end
   end
