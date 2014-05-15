7aa5d603d5347dd63f23555fb923ef8f70ed36fe
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-05-15 20:53:09 +0000

    changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,10 +4,12 @@ class DashboardController < ApplicationController
 
   def index
     @user = User.find(session[:user_id])
+    InventoryException.all.destroy
+    InventoryPosition.all.each {|ip| ip.generate_inventory_exceptions}
     @locations = @user.organization.locations
     @data_array = [] 
     @locations.each do |location|
-        @data_array << [location.latitude, location.longitude, location.inventory_exceptions.count, location.name]
+        @data_array << [location.latitude, location.longitude, location.inventory_exceptions.count]
     end
   end
 
