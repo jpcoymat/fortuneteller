dbc8c9d0913c6679a5c080c0ac94dee105dc7da4
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-05-15 22:23:28 +0000

    changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,9 +7,9 @@ class DashboardController < ApplicationController
     InventoryException.all.destroy
     InventoryPosition.all.each {|ip| ip.generate_inventory_exceptions}
     @locations = @user.organization.locations
-    @data_array = "" 
+    @data_array = [] 
     @locations.each do |location|
-        @data_array += ",[" + location.latitude.to_s + ", " + location.longitude.to_s + ", " + location.inventory_exceptions.count.to_s + "]"
+        @data_array << [location.latitude, location.longitude, location.name, location.inventory_exceptions.where(priority: 1).count, location.inventory_exceptions.where(priority: 2).count]
     end
   end
 
