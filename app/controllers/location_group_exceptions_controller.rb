e7bab5b4c36cbc795aa3f5c4decd1c7d40da77d0
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-05-22 19:49:45 +0000

    changes

--- app/controllers/location_group_exceptions_controller.rb
+++ app/controllers/location_group_exceptions_controller.rb
@@ -7,6 +7,32 @@ class LocationGroupExceptionsController < ApplicationController
     @user = User.find(session[:user_id])
     @organization = @user.organization
     @location_group_exceptions = LocationGroupException.where(location_group_exception_params)
+
+    @exception_durations = []
+    @exception_days_to_impact = []
+    @location_group_exceptions.each do |location_group_exception|
+      @exception_durations << location_group_exception.duration
+      @exception_days_to_impact << location_group_exception.days_to_impact
+    end
+    @max_duration = @exception_durations.max
+    @max_days_to_impact = @exception_days_to_impact.max
+    @min_days_to_impact = @exception_days_to_impact.min
+    @color_increment = ((@max_days_to_impact - @min_days_to_impact)/3).floor
+    @color_limits = {
+                     "red_end" => @min_days_to_impact + @color_increment,
+                     "yellow_end" => @min_days_to_impact + 2*@color_increment}
+    @exception_colors = {}
+    @location_group_exceptions.each do |location_group_exception|
+      if location_group_exception.days_to_impact <= @color_limits["red_end"]
+        @exception_colors[location_group_exception.id] = "red"
+      elsif  location_group_exception.days_to_impact <= @color_limits["yellow_end"]
+        @exception_colors[location_group_exception.id] = "yellow"
+       else
+         @exception_colors[location_group_exception.id] = "green"
+      end
+    end
+
+
   end
 
   protected
--- app/models/location_group_exception.rb
+++ app/models/location_group_exception.rb
@@ -14,6 +14,14 @@ class LocationGroupException
 
   validates :product, :begin_date, :end_date, :aggregate_minimum, :aggregate_on_hand_quantity, presence: true
 
+ def duration
+    @duration = (self.end_date - self.begin_date).to_i
+  end
+
+
+  def days_to_impact
+    @days_to_impact = [(self.begin_date - Date.today).to_i, 0].max
+  end
 
 
 end
